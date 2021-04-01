aa113dd9dc54f28f18b66ba05cf2b833c98a98e3
Randy Heiland <heiland@indiana.edu>
2021-04-01 05:03:20 -0400

    updates

--- qt_for_python/test_tabs/gui4xml.py
+++ qt_for_python/test_tabs/gui4xml.py
@@ -11,7 +11,7 @@ Dr. Paul Macklin (macklinp@iu.edu)
 
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtWidgets import QFrame,QApplication,QWidget,QTabWidget,QFormLayout,QLineEdit, QHBoxLayout,QVBoxLayout,QRadioButton,QLabel,QCheckBox,QComboBox
+from PySide6.QtWidgets import QFrame,QApplication,QWidget,QTabWidget,QFormLayout,QLineEdit, QHBoxLayout,QVBoxLayout,QRadioButton,QLabel,QCheckBox,QComboBox, QMenuBar,QStyle,QGridLayout 
 
 from config_tab import Config
 from cell_def_tab import CellDef 
@@ -21,54 +21,53 @@ from microenv_tab import SubstrateDef
 class PhysiCellXMLCreator(QTabWidget):
     def __init__(self, parent = None):
         super(PhysiCellXMLCreator, self).__init__(parent)
-        self.tab1 = QWidget()
-        self.tab2 = QWidget()
-        # self.tab3 = QWidget()
+
+        self.setWindowTitle("PhysiCell configuration")
+
+        # Menus
+        lay = QVBoxLayout(self)
+        lay.setContentsMargins(5, 35, 5, 5)
+        self.menu()
+        self.setWindowIcon(self.style().standardIcon(getattr(QStyle, 'SP_DialogNoButton')))
+        self.grid = QGridLayout()
+        lay.addLayout(self.grid)
+        self.setLayout(lay)
+        self.setMinimumSize(400, 320)
+
+        # self.menubar = QtWidgets.QMenuBar(self)
+        # self.file_menu = QtWidgets.QMenu('File')
+        # self.file_menu.insertAction("Open")
+        # self.menubar.addMenu(self.file_menu)
+
+        # GUI tabs
         self.tab1 = Config()
         self.tab2 = SubstrateDef()
         self.tab3 = CellDef()
-        # self.cell_defs = CellDefInstances()
 
         self.addTab(self.tab1,"Config Basics")
         self.addTab(self.tab2,"Microenvironment")
         self.addTab(self.tab3,"Cell Types")
-        # self.addTab(self.cell_defs,"Cell Defs")
-        # self.tab_config()
-        # self.tab_microenv()
-        # self.tab_cell_defs()
-        self.setWindowTitle("PhysiCell configuration")
+
+
+    def menu(self):
+        menubar = QMenuBar(self)
+
+        file_menu = menubar.addMenu('File')
+        open_act = QtGui.QAction('Open', self)
+        biorobots = QtGui.QAction('Load biorobots', self)
+        save_act = QtGui.QAction('Save', self)
+        saveas_act = QtGui.QAction('Save As', self)
+        # open_act = QtGui.QAction('Open', self, checkable=True)
+        file_menu.setStatusTip('enable/disable Dark mode')
+        file_menu.addAction(open_act)
+        file_menu.addAction(biorobots)
+        file_menu.addAction(save_act)
+        file_menu.addAction(saveas_act)
+
+        tools_menu = menubar.addMenu('Tools')
+        email = QtGui.QAction('Validate', self)
+        tools_menu.addAction(email)
 		
-    def tab_microenv(self):
-        layout = QFormLayout()
-        sex = QHBoxLayout()
-        sex.addWidget(QRadioButton("Male"))
-        sex.addWidget(QRadioButton("Female"))
-        layout.addRow(QLabel("Sex"),sex)
-        layout.addRow("Date of Birth",QLineEdit())
-        # self.setTabText(1,"Microenvironment")
-        self.tab2.setLayout(layout)
-
-    @QtCore.Slot()
-    def save_xml(self):
-        # self.text.setText(random.choice(self.hello))
-        pass
-
-    @QtCore.Slot()
-    def cycle_changed_cb(self, idx):
-        pass
-        # QtWidgets.QMessageBox.information(self, "Cycle Changed:",
-                #   "Current Cycle Index: %d" % idx )
-
-    @QtCore.Slot()
-    def tab_changed_cb(self, idx):
-        # self.text.setText(random.choice(self.hello))
-        # pass
-        QtWidgets.QMessageBox.information(self, "Tab Index Changed!",
-                  "Current Tab Index: %d" % idx )
-        # if idx == 2:
-        #     self.scroll.setWidget(self.params_cell_def)
-        # else:
-        #     self.scroll.setWidget(self.params_config)
 
 def main():
     app = QApplication(sys.argv)
